eb00053b17172db3690f6c91394343c9d5e89652
irow0433 <31513820+irow0433@users.noreply.github.com>
2017-09-25 08:31:51 -0600

    added Int to swift and names

--- LearningSwift.playground/Contents.swift
+++ LearningSwift.playground/Contents.swift
@@ -1,5 +1,31 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+//Explicit Type declaration with no value
+var ramen :String
+//Implicit type declaration with a value to assign type.
 
-var str = "Hello, playground"
+var soup = "Ramen"
+ramen = soup
+print(ramen)
+print(soup)
+//ramen = 7
+var myAge :Int
+myAge = -89
+print(myAge)
+myAge = myAge * 33421
+var someValue = 3.1456 * 56
+someValue = someValue * 231.1
+var isReal :Bool
+isReal = false
+isReal = true
+let myName :String
+myName = "Isaac"
+print(myName)
+//myName = soup
+//can't assign into a let once assigned
+
+
+
+
+
