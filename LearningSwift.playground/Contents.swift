83b741efae5f182525d614bd2e014524db9c4c10
irow0433 <31513820+irow0433@users.noreply.github.com>
2017-09-25 09:17:39 -0600

    added statements to make a sentence

--- LearningSwift.playground/Contents.swift
+++ LearningSwift.playground/Contents.swift
@@ -5,29 +5,56 @@ import UIKit
 var ramen :String
 //Implicit type declaration with a value to assign type.
 
-var soup = "Ramen"
-ramen = soup
-print(ramen)
-print(soup)
+//var soup = "Ramen"
+//ramen = soup
+//print(ramen)
+//print(soup)
 //ramen = 7
-var myAge :Int
-myAge = -89
-print(myAge)
-myAge = myAge * 33421
-var someValue = 3.1456 * 56
-someValue = someValue * 231.1
-var isReal :Bool
-isReal = false
-isReal = true
-let myName :String
-myName = "Isaac"
-print(myName)
+//var myAge :Int
+//myAge = -89
+//print(myAge)
+//myAge = myAge * 33421
+//var someValue = 3.1456 * 56
+//someValue = someValue * 231.1
+//var isReal :Bool
+//isReal = false
+//isReal = true
+//let myName :String
+//myName = "Isaac"
+//print(myName)
 //myName = soup
 //can't assign into a let once assigned
-let mySentence :String
-mySentence = "This is a test"
-print(mySentence)
+//let mySentence :String
+//mySentence = "This is a test"
+//print(mySentence)
 
+//var name :String
+//let myHouse :String
+//myHouse = "Is this my house?"
+//print(myHouse)
+//var AnswerTrue = "This is my House!!!"
+//var AnswerFalse = "Oh No! Where is my house?!"
+//print(myHouse)
+//var house :Bool
+//house = true
+//print(house)
+//print(AnswerTrue)
+
+var house :Bool
+var q :String
+q = "Is this my House?!"
+print(q)
+house = true
+print("yes")
+if house == true
+{
+    print("Yay I found my house")
+}
+
+else
+{
+    print("Oh No, that is not my house!")
+}
 
 
 
